85bf36de99f0e3b5ec02e042501f6d832facb707
maziyar <mazyar.gh83@gmail.com>
2024-02-12 18:03:37 +0330

    add color picker

---
+++ Reminder/Views/AddNewListView.swift
@@ -0,0 +1,18 @@
+//
+//  AddNewListView.swift
+//  Reminder
+//
+//  Created by kh45 on 2/12/24.
+//
+
+import SwiftUI
+
+struct AddNewListView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddNewListView()
+}
